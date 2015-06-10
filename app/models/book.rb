fd4878ac00dddbac356b074a64a4eee5294808d0
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-10 16:55:47 -0700

    initial commit

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ActiveRecord::Base
   belongs_to :author
+  has_many :books_libraries
+  has_many :libraries, through: :books_libraries
+  has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609234706) do
+ActiveRecord::Schema.define(version: 20150610003200) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,21 @@ ActiveRecord::Schema.define(version: 20150609234706) do
     t.integer  "author_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "books_libraries", force: :cascade do |t|
+    t.integer  "library_id"
+    t.integer  "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "libraries", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.string   "state"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
