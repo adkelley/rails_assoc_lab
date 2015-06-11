8f1db4666a4310920160fe8395fd6a7b6bfa76c0
Alex Kelley <alexkelley@Alexs-MacBook-Pro.local>
2015-06-11 07:07:12 -0700

    Added email column to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611001442) do
+ActiveRecord::Schema.define(version: 20150611140239) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20150611001442) do
     t.datetime "updated_at",            null: false
     t.string   "password"
     t.string   "password_confirmation"
+    t.string   "email"
   end
 
 end
